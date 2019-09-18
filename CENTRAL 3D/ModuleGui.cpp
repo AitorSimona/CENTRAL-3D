557861453e7819da45762376562fce4c824982b7
Aitor Simona Bouzas <aulazsimonaaitor@gmail.com>
2019-09-18 22:58:14 +0200

    Imgui MenuBar with Quit button

--- CENTRAL 3D/ModuleGui.cpp
+++ CENTRAL 3D/ModuleGui.cpp
@@ -9,6 +9,7 @@
 #include "Source/imgui/imgui_impl_opengl2.h"
 
 static bool show_demo_window = true;
+bool show_another_window = true;
 
 ModuleGui::ModuleGui(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -54,6 +55,69 @@ update_status ModuleGui::PreUpdate(float dt)
 
 update_status ModuleGui::Update(float dt)
 {
+	// Menu Bar
+	/*if (ImGui::BeginMenuBar())
+	{
+		if (ImGui::BeginMenu("Menu"))
+		{
+			ImGui::EndMenu();
+		}
+		if (ImGui::BeginMenu("Examples"))
+		{
+			ImGui::MenuItem("Quit", NULL);
+			ImGui::EndMenu();
+		}
+		if (ImGui::MenuItem("Quit", "Alt+F4"))
+		{
+
+		}
+
+		ImGui::EndMenuBar();
+
+	}*/
+
+	if (show_another_window)
+	{
+		if (ImGui::BeginMainMenuBar())
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::Button("Quit"))
+				{
+					return UPDATE_STOP;
+				}
+				ImGui::EndMenu();
+			}
+			ImGui::EndMainMenuBar();
+		}
+
+		//ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+		//ImGui::Text("Hello from another window!");
+		//if (ImGui::Button("Close Me"))
+		//	show_another_window = false;
+		//ImGui::End();
+	}
+
+	//if (ImGui::BeginMainMenuBar())
+	//{
+	//	if (ImGui::BeginMenu("File"))
+	//	{
+	//		ShowExampleMenuFile();
+	//		ImGui::EndMenu();
+	//	}
+	//	if (ImGui::BeginMenu("Edit"))
+	//	{
+	//		if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
+	//		if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
+	//		ImGui::Separator();
+	//		if (ImGui::MenuItem("Cut", "CTRL+X")) {}
+	//		if (ImGui::MenuItem("Copy", "CTRL+C")) {}
+	//		if (ImGui::MenuItem("Paste", "CTRL+V")) {}
+	//		ImGui::EndMenu();
+	//	}
+	//	ImGui::EndMainMenuBar();
+	//}
+
 
 	if (show_demo_window)
 		ImGui::ShowDemoWindow(&show_demo_window);
