fc64a0d847fb707599820efa3b68904735bd06a0
Aitor Simona Bouzas <aulazsimonaaitor@gmail.com>
2019-09-19 18:30:02 +0200

    Removed old containers, adapted to STL

--- CENTRAL 3D/Application.cpp
+++ CENTRAL 3D/Application.cpp
@@ -30,12 +30,12 @@ Application::Application()
 
 Application::~Application()
 {
-	p2List_item<Module*>* item = list_modules.getLast();
+	std::list<Module*>::reverse_iterator item = list_modules.rbegin();
 
-	while(item != NULL)
+	while(item != list_modules.rend())
 	{
-		delete item->data;
-		item = item->prev;
+		delete *item;
+		item++;
 	}
 }
 
@@ -44,25 +44,26 @@ bool Application::Init()
 	bool ret = true;
 
 	// Call Init() in all modules
-	p2List_item<Module*>* item = list_modules.getFirst();
+	std::list<Module*>::const_iterator item = list_modules.begin();
 
-	while(item != NULL && ret == true)
+	while(item != list_modules.end() && ret == true)
 	{
-		ret = item->data->Init();
-		item = item->next;
+		ret = (*item)->Init();
+		item++;
 	}
 
 	// After all Init calls we call Start() in all modules
 	LOG("Application Start --------------");
-	item = list_modules.getFirst();
+	item = list_modules.begin();
 
-	while(item != NULL && ret == true)
+	while(item != list_modules.end() && ret == true)
 	{
-		ret = item->data->Start();
-		item = item->next;
+		ret = (*item)->Start();
+		item++;
 	}
 	
 	ms_timer.Start();
+
 	return ret;
 }
 
@@ -84,48 +85,49 @@ update_status Application::Update()
 	update_status ret = UPDATE_CONTINUE;
 	PrepareUpdate();
 	
-	p2List_item<Module*>* item = list_modules.getFirst();
+	std::list<Module*>::const_iterator item = list_modules.begin();
 	
-	while(item != NULL && ret == UPDATE_CONTINUE)
+	while(item != list_modules.end() && ret == UPDATE_CONTINUE)
 	{
-		ret = item->data->PreUpdate(dt);
-		item = item->next;
+		ret = (*item)->PreUpdate(dt);
+		item++;
 	}
 
-	item = list_modules.getFirst();
+	item = list_modules.begin();
 
-	while(item != NULL && ret == UPDATE_CONTINUE)
+	while(item != list_modules.end() && ret == UPDATE_CONTINUE)
 	{
-		ret = item->data->Update(dt);
-		item = item->next;
+		ret = (*item)->Update(dt);
+		item++;
 	}
 
-	item = list_modules.getFirst();
+	item = list_modules.begin();
 
-	while(item != NULL && ret == UPDATE_CONTINUE)
+	while(item != list_modules.end() && ret == UPDATE_CONTINUE)
 	{
-		ret = item->data->PostUpdate(dt);
-		item = item->next;
+		ret = (*item)->PostUpdate(dt);
+		item++;
 	}
 
 	FinishUpdate();
+
 	return ret;
 }
 
 bool Application::CleanUp()
 {
 	bool ret = true;
-	p2List_item<Module*>* item = list_modules.getLast();
+	std::list<Module*>::reverse_iterator item = list_modules.rbegin();
 
-	while(item != NULL && ret == true)
+	while(item != list_modules.rend() && ret == true)
 	{
-		ret = item->data->CleanUp();
-		item = item->prev;
+		ret = (*item)->CleanUp();
+		item++;
 	}
 	return ret;
 }
 
 void Application::AddModule(Module* mod)
 {
-	list_modules.add(mod);
+	list_modules.push_back(mod);
 }
--- CENTRAL 3D/Application.h
+++ CENTRAL 3D/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "p2List.h"
+#include <list>
+
 #include "Globals.h"
 #include "Timer.h"
 #include "Module.h"
@@ -25,7 +26,7 @@ private:
 
 	Timer	ms_timer;
 	float	dt;
-	p2List<Module*> list_modules;
+	std::list<Module*> list_modules;
 
 public:
 
