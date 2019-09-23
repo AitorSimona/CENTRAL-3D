8c36bf794677d2abde68fbd12bc08c4d41ae74d4
Aitor Simona Bouzas <aulazsimonaaitor@gmail.com>
2019-09-23 15:55:10 +0200

    Basic Docking working!
    Now looking into viewports, still cannot combine with docking

--- CENTRAL 3D/ModuleGui.cpp
+++ CENTRAL 3D/ModuleGui.cpp
@@ -7,9 +7,11 @@
 #include "Source/Imgui/imgui.h"
 #include "Source/imgui/imgui_impl_sdl.h"
 #include "Source/imgui/imgui_impl_opengl3.h"
+#include "Source/Imgui/imgui_internal.h"
+#include "Source/Imgui/ImGuizmo/ImGuizmo.h"
 
 static bool show_demo_window = true;
-bool show_another_window = true;
+static bool show_another_window = true;
 
 ModuleGui::ModuleGui(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -33,7 +35,8 @@ bool ModuleGui::Start()
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable keyboard controls
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; // Enable Window Docking (Under Active Development)
+	/*io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;*/
 
 	ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->renderer3D->context);
 	ImGui_ImplOpenGL3_Init();
@@ -47,15 +50,22 @@ bool ModuleGui::Start()
 update_status ModuleGui::PreUpdate(float dt)
 {  
 	// Start the frame
+
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui_ImplSDL2_NewFrame(App->window->window);
 	ImGui::NewFrame();
+
+	//ImGuizmo::BeginFrame();
+
+	// Begin dock space
+	DockSpace();
 
 	return UPDATE_CONTINUE;
 }
 
 update_status ModuleGui::Update(float dt)
 {
+
 	// Menu Bar
 	/*if (ImGui::BeginMenuBar())
 	{
@@ -128,6 +138,9 @@ update_status ModuleGui::Update(float dt)
 
 update_status ModuleGui::PostUpdate(float dt)
 {
+	// End dock space
+	ImGui::End();
+
 	return UPDATE_CONTINUE;
 }
 
@@ -147,4 +160,24 @@ void ModuleGui::Draw() const
 	// Render
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+	//if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	//{
+	//	ImGui::UpdatePlatformWindows();
+	//	ImGui::RenderPlatformWindowsDefault();
+	//}
+}
+
+void ModuleGui::DockSpace() const
+{
+	ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+	window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+	window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+	static bool p_open = true;
+	ImGui::Begin("DockSpace Demo", &p_open, window_flags);
+
+	ImGuiID dockspace_id = ImGui::GetID("MyDockspace");
+	ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
+	ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
 }
--- CENTRAL 3D/ModuleGui.h
+++ CENTRAL 3D/ModuleGui.h
@@ -19,7 +19,7 @@ public:
 	bool CleanUp();
 
 	void Draw() const;
-
+	void DockSpace() const;
 };
 
 #endif
