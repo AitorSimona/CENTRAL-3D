0c3128c22f605e57adb1656dc485db4abf7bba26
Aitor Simona Bouzas <aulazsimonaaitor@gmail.com>
2019-10-24 22:22:32 +0200

    Corrected Focus error, camera main movements working properly!!

--- CENTRAL 3D/Source/ModuleCamera3D.cpp
+++ CENTRAL 3D/Source/ModuleCamera3D.cpp
@@ -50,10 +50,8 @@ update_status ModuleCamera3D::Update(float dt)
 	if(App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
 		speed = 20.0f * dt;
 
-	if (!App->gui->IsKeyboardCaptured())
+	if (!App->gui->IsMouseCaptured() && App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
 	{
-		/*if (App->input->GetKey(SDL_SCANCODE_R) == KEY_REPEAT) newPos.y += speed;
-		if (App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT) newPos.y -= speed;*/
 
 		if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos -= Z * speed;
 		if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos += Z * speed;
@@ -121,43 +119,43 @@ update_status ModuleCamera3D::Update(float dt)
 
 	// Mouse motion ----------------
 
-	//if(!App->gui->IsMouseCaptured() && App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
-	//{
-	//	int dx = -App->input->GetMouseXMotion();
-	//	int dy = -App->input->GetMouseYMotion();
+	if(!App->gui->IsMouseCaptured() && App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
+	{
+		int dx = -App->input->GetMouseXMotion();
+		int dy = -App->input->GetMouseYMotion();
 
-	//	float Sensitivity = 0.25f;
+		float Sensitivity = 0.25f;
 
-	//	//vec3 Reference_backup = Reference;
-	//	Reference = Position;
-	//	Position -= Reference;
+		//vec3 Reference_backup = Reference;
+		Reference = Position;
+		Position -= Reference;
 
-	//	if(dx != 0)
-	//	{
-	//		float DeltaX = (float)dx * Sensitivity;
+		if(dx != 0)
+		{
+			float DeltaX = (float)dx * Sensitivity;
 
-	//		X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-	//		Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-	//		Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-	//	}
+			X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+			Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+			Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+		}
 
-	//	if(dy != 0)
-	//	{
-	//		float DeltaY = (float)dy * Sensitivity;
+		if(dy != 0)
+		{
+			float DeltaY = (float)dy * Sensitivity;
 
-	//		Y = rotate(Y, DeltaY, X);
-	//		Z = rotate(Z, DeltaY, X);
+			Y = rotate(Y, DeltaY, X);
+			Z = rotate(Z, DeltaY, X);
 
-	//		if(Y.y < 0.0f)
-	//		{
-	//			Z = vec3(0.0f, Z.y > 0.0f ? 1.0f : -1.0f, 0.0f);
-	//			Y = cross(Z, X);
-	//		}
-	//	}
+			if(Y.y < 0.0f)
+			{
+				Z = vec3(0.0f, Z.y > 0.0f ? 1.0f : -1.0f, 0.0f);
+				Y = cross(Z, X);
+			}
+		}
 
-	//	Position = Reference + Z * length(Position);
-	//	//Reference = Reference_backup;
-	//}
+		Position = Reference + Z * length(Position);
+		Reference = Position - Z * length(Position);
+	}
 
 
 
