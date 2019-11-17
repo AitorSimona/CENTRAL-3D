44e3f09ec7089dc979b4759565b1daf909a5b119
Aitor Simona Bouzas <aulazsimonaaitor@gmail.com>
2019-11-17 21:59:37 +0100

    ComponentCamera basic functionality added

--- CENTRAL 3D/Source/ComponentCamera.cpp
+++ CENTRAL 3D/Source/ComponentCamera.cpp
@@ -1,9 +1,128 @@
 #include "ComponentCamera.h"
 
+
 ComponentCamera::ComponentCamera(GameObject* ContainerGO) : Component(ContainerGO, Component::ComponentType::Camera)
 {
+	frustum.type = FrustumType::PerspectiveFrustum;
+
+	frustum.pos = (float3(0, 0, 0));
+	frustum.front = (float3::unitZ);
+	frustum.up = (float3::unitY);
+
+	frustum.nearPlaneDistance = 1.0f;
+	frustum.farPlaneDistance = 1000.0f;
+	SetAspectRatio(1.0f);
+
+	UpdatePlanes();
+	update_projection = true;
 }
 
 ComponentCamera::~ComponentCamera()
 {
+}
+
+float ComponentCamera::GetNearPlane() const
+{
+	return frustum.nearPlaneDistance;
+}
+
+float ComponentCamera::GetFarPlane() const
+{
+	return frustum.farPlaneDistance;
+}
+
+float ComponentCamera::GetFOV() const
+{
+	return frustum.verticalFov * RADTODEG;
+}
+
+float ComponentCamera::GetAspectRatio() const
+{
+	return frustum.AspectRatio();
+}
+
+float4x4 ComponentCamera::GetOpenGLViewMatrix()
+{
+	math::float4x4 matrix = frustum.ViewMatrix();
+	return matrix.Transposed();
+}
+
+float4x4 ComponentCamera::GetOpenGLProjectionMatrix()
+{
+	math::float4x4 matrix = frustum.ProjectionMatrix();
+	return matrix.Transposed();
+}
+
+void ComponentCamera::SetNearPlane(float distance)
+{
+	if (distance > 0 && distance < frustum.farPlaneDistance)
+	{
+		frustum.nearPlaneDistance = distance;
+		UpdatePlanes();
+		update_projection = true;
+	}
+
+}
+
+void ComponentCamera::SetFarPlane(float distance)
+{
+	if (distance > 0 && distance > frustum.nearPlaneDistance)
+	{
+		frustum.farPlaneDistance = distance;
+		UpdatePlanes();
+		update_projection = true;
+	}
+}
+
+void ComponentCamera::SetFOV(float fov)
+{
+	fov *= DEGTORAD;
+
+	frustum.horizontalFov = fov;
+	frustum.verticalFov = 2.f * Atan(Tan(fov*0.5f) / frustum.AspectRatio());
+
+	UpdatePlanes();
+	update_projection = true;
+}
+
+void ComponentCamera::SetAspectRatio(float ar)
+{
+	float horizontalFov = frustum.horizontalFov;
+	frustum.horizontalFov = horizontalFov;
+	frustum.verticalFov = 2.f * Atan(Tan(horizontalFov*0.5f) / frustum.AspectRatio());
+	UpdatePlanes();
+	update_projection = true;
+}
+
+void ComponentCamera::Look(const float3 & position)
+{
+	float3 vector = position - frustum.pos;
+
+	float3x3 matrix = float3x3::LookAt(frustum.front, vector.Normalized(), frustum.up, float3::unitY);
+
+	frustum.front = matrix.MulDir(frustum.front).Normalized();
+	frustum.up = matrix.MulDir(frustum.up).Normalized();
+	UpdatePlanes();
+	update_projection = true;
+}
+
+void ComponentCamera::OnUpdateTransform(const float4x4 & global)
+{
+	frustum.front = global.WorldZ();
+	frustum.up = global.WorldY();
+
+	float3 position = float3::zero;
+	float3 scale = float3::one;
+	Quat quat = Quat::identity;
+	global.Decompose(position, quat, scale);
+
+	frustum.pos = position;
+	UpdatePlanes();
+
+	update_projection = true;
+}
+
+void ComponentCamera::UpdatePlanes()
+{
+	frustum.GetPlanes(planes);
 }
--- CENTRAL 3D/Source/ComponentCamera.h
+++ CENTRAL 3D/Source/ComponentCamera.h
@@ -2,6 +2,7 @@
 #define __COMPONENT_CAMERA_H__
 
 #include "Component.h"
+#include "Math.h"
 
 class ComponentCamera : public Component
 {
@@ -10,6 +11,33 @@ public:
 	ComponentCamera(GameObject* ContainerGO);
 	virtual ~ComponentCamera();
 
+	// --- Getters ---
+	float GetNearPlane() const;
+	float GetFarPlane() const;
+	float GetFOV() const;
+	float GetAspectRatio() const;
+	float4x4 GetOpenGLViewMatrix();
+	float4x4 GetOpenGLProjectionMatrix();
+
+	// --- Setters ---
+	void SetNearPlane(float distance);
+	void SetFarPlane(float distance);
+	void SetFOV(float fov);
+	void SetAspectRatio(float ar);
+	
+	// --- Utilities ---
+	void Look(const float3& position);
+	void OnUpdateTransform(const float4x4& global);
+
+private:
+	void UpdatePlanes();
+
+public:
+	Frustum		frustum;
+	bool		update_projection = true;
+	bool		culling = false;
+	bool		active_camera = false;
+	Plane		planes[6];
 };
 
 #endif
