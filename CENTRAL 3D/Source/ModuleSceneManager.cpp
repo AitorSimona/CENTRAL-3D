2ab85799647907557f914f3b7d836a3086d82335
Aitor Simona Bouzas <aulazsimonaaitor@gmail.com>
2019-12-03 19:31:38 +0100

    Eliminated unnecesary stuff, creating default shader program (currently at scene manager, will be moved)
    also using baker house again for testing

--- CENTRAL 3D/Source/ModuleSceneManager.cpp
+++ CENTRAL 3D/Source/ModuleSceneManager.cpp
@@ -69,6 +69,76 @@ bool ModuleSceneManager::Start()
 	cube = CreateCube(1, 1, 1);
 	sphere = CreateSphere(1.0f, 25, 25);
 
+	// --- Creating Default Vertex and Fragment Shaders ---
+	const char vertexShaderSource[] =
+	"#version 330 core \n"
+	"layout (location = 0) in vec3 position; \n"
+	"layout(location = 1) in vec3 color; \n"
+	"layout (location = 2) in vec2 texCoord; \n"
+	"out vec3 ourColor; \n"
+	"out vec2 TexCoord; \n"
+	"uniform mat4 model_matrix; \n"
+	"uniform mat4 view; \n"
+	"uniform mat4 projection; \n"
+	"void main(){ \n"
+	"gl_Position = projection * view * model_matrix * vec4(position, 1.0f); \n"
+	"ourColor = color; \n"
+	"TexCoord = texCoord; \n"
+	"}\n"
+	;
+	
+	const char fragmentShaderSource [] =
+	"#version 330 core \n"
+	"in vec3 ourColor; \n"
+	"in vec2 TexCoord; \n"
+	"out vec4 color; \n"
+	"uniform sampler2D ourTexture; \n"
+	"void main(){ \n"
+	"color = texture(ourTexture, TexCoord); \n"
+	"} \n"
+	;
+
+	GLchar const* vertexSource = vertexShaderSource;
+	GLchar const* fragmentSource = fragmentShaderSource;
+
+	// --- Creating GL Shaders from given code and compiling  ---
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (success == 0)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		CONSOLE_LOG("Vertex Shader compilation error: %s", infoLog);
+	}
+	//glDeleteShader(vertexShader);
+
+	GLuint fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (success == 0)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		CONSOLE_LOG("Fragment Shader compilation error: %s", infoLog);
+	}
+	//glDeleteShader(vertexShader);
+
+	// --- Creating Shader program and linking both vertex and fragment to it ---
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		CONSOLE_LOG("Shader link error: %s", infoLog);
+	}
 
 	return true;
 }
