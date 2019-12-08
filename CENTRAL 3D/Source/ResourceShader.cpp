011c4482829edff39ce539fd08693220504c045a
Aitor Simona Bouzas <aulazsimonaaitor@gmail.com>
2019-12-08 19:20:27 +0100

    Filled ResourceShader with Constructor/Setters/Use functions

--- CENTRAL 3D/Source/ResourceShader.cpp
+++ CENTRAL 3D/Source/ResourceShader.cpp
@@ -1,7 +1,95 @@
+#include <fstream>
+#include <sstream>
 #include "ResourceShader.h"
+
+#include "OpenGL.h"
+
+#include "mmgr/mmgr.h"
 
 ResourceShader::ResourceShader() : Resource(Resource::ResourceType::SHADER)
 {
+}
+
+ResourceShader::ResourceShader(const char * vertexPath, const char * fragmentPath) : Resource(Resource::ResourceType::SHADER)
+{
+	// MYTODO: Clean this and use physFS
+
+	// 1. retrieve the vertex/fragment source code from filePath
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
+	// ensure ifstream objects can throw exceptions:
+	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	try
+	{
+		// open files
+		vShaderFile.open(vertexPath);
+		fShaderFile.open(fragmentPath);
+		std::stringstream vShaderStream, fShaderStream;
+		// read file's buffer contents into streams
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
+		// close file handlers
+		vShaderFile.close();
+		fShaderFile.close();
+		// convert stream into string
+		vertexCode = vShaderStream.str();
+		fragmentCode = fShaderStream.str();
+	}
+	catch (std::ifstream::failure e)
+	{
+		CONSOLE_LOG("|[error]:SHADER:FILE_NOT_SUCCESFULLY_READ: %s", e.what());
+	}
+	const char* vShaderCode = vertexCode.c_str();
+	const char* fShaderCode = fragmentCode.c_str();
+
+	// 2. compile shaders
+	unsigned int vertex, fragment;
+	int success;
+	char infoLog[512];
+
+	// vertex Shader
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vShaderCode, NULL);
+	glCompileShader(vertex);
+	// print compile errors if any
+	glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+		CONSOLE_LOG("|[error]:Vertex Shader compilation error: %s", infoLog);
+	};
+
+	// similiar for Fragment Shader
+	fragment = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(fragment, 1, &fShaderCode, NULL);
+	glCompileShader(fragment);
+	// print compile errors if any
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+		CONSOLE_LOG("|[error]:Fragment Shader compilation error: %s", infoLog);
+	};
+
+	// shader Program
+	ID = glCreateProgram();
+	glAttachShader(ID, vertex);
+	glAttachShader(ID, fragment);
+	glLinkProgram(ID);
+	// print linking errors if any
+	glGetProgramiv(ID, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(ID, 512, NULL, infoLog);
+		CONSOLE_LOG("|[error]:SHADER::PROGRAM::LINKING_FAILED: %s", infoLog);
+	}
+
+	// delete the shaders as they're linked into our program now and no longer necessery
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
 }
 
 ResourceShader::~ResourceShader()
@@ -14,4 +102,24 @@ void ResourceShader::LoadInMemory()
 
 void ResourceShader::FreeMemory()
 {
+}
+
+void ResourceShader::use()
+{
+	glUseProgram(ID);
+}
+
+void ResourceShader::setBool(const std::string & name, bool value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void ResourceShader::setInt(const std::string & name, int value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void ResourceShader::setFloat(const std::string & name, float value) const
+{
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 }
