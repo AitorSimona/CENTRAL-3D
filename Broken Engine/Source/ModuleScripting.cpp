c95dbb001fc94df422212e0a0a1f49a9aea5feb1
lucho1 <luchosuaya@hotmail.com>
2020-03-02 19:13:55 +0100

    Update ModuleScripting.cpp

--- Broken Engine/Source/ModuleScripting.cpp
+++ Broken Engine/Source/ModuleScripting.cpp
@@ -226,8 +226,8 @@ void ModuleScripting::CompileScriptTableClass(ScriptInstance * script)
 		
 		.addFunction("PlayAttackSound", &Scripting::PlayAttackSound)
 		.addFunction("PlayStepSound", &Scripting::PlayStepSound)
-		.addFunction("PlayAttackSound", &Scripting::StopAttackSound)
-		.addFunction("PlayStepSound", &Scripting::StopStepSound)
+		.addFunction("StopAttackSound", &Scripting::StopAttackSound)
+		.addFunction("StopStepSound", &Scripting::StopStepSound)
 		.addFunction("SetVolume", &Scripting::SetVolume)
 
 		.addFunction("PlayAnimation", &Scripting::StartAnimation)
