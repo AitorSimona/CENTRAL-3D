cb15293af3eeae2cd8ef931e7c594a18df79b45a
Aitor Simona Bouzas <simonaaitor@gmail.com>
2020-03-19 00:27:16 +0100

    added support for resource inspector info display

--- Broken Engine/Source/Resource.h
+++ Broken Engine/Source/Resource.h
@@ -61,6 +61,7 @@ public:
 
 	virtual void OnOverwrite() = 0;
 	virtual void OnDelete() = 0;
+	virtual void CreateInspectorNode() {};
 
 	// to encapsulate model childs in panelproject
 	bool has_parent = false;
@@ -69,6 +70,7 @@ protected:
 	virtual bool LoadInMemory() = 0;
 	virtual void FreeMemory() = 0;
 	virtual void Repath() {};
+
 
 
 protected:
