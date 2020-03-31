376a6b72fc3023564775e230226acea82604c5a7
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2020-03-31 17:04:16 +0200

    Changed bin to bin_lua so it goes through the gitignore

--- Broken Engine/Game/Lua_Debug/IDE_Debugger.lua
+++ Broken Engine/Game/Lua_Debug/IDE_Debugger.lua
@@ -1,16 +1,8 @@
---set ZBS=C:\Users\didac\Downloads\ZeroBraneStudio
---set LUA_PATH=./?.lua;%ZBS%/lualibs/?/?.lua;%ZBS%/lualibs/?.lua
---set LUA_CPATH=%ZBS%/bin/?.dll;%ZBS%/bin/clibs/?.dll
-
--- ZBS = "C:/Users/didac/Downloads/ZeroBraneStudio"
--- package.path = package.path .. "./?.lua;" .. ZBS .. "/lualibs/?/?.lua;" .. ZBS .. "/lualibs/?.lua"
--- package.cpath = package.cpath .. ZBS .. "/bin/?.dll;" .. ZBS .. "/bin/clibs53/?.dll"
---require('socket')
 local System = Scripting.System()
 
 ZBS = System:GetDebuggingPath()
 package.path = package.path .. "./?.lua;" .. ZBS .. "/lualibs/?/?.lua;" .. ZBS .. "/lualibs/?.lua"
-package.cpath = package.cpath .. ZBS .. "/bin/?.dll;" .. ZBS .. "/bin/clibs53/?.dll"
+package.cpath = package.cpath .. ZBS .. "/bin_lua/?.dll;" .. ZBS .. "/bin_lua/clibs53/?.dll"
 
 DEBUG = require('mobdebug')
 
